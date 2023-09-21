5f2c21f68529835b019d48191579fdbd8057ecfc
Pavset <79660475+Pavset@users.noreply.github.com>
2023-09-21 18:58:47 +0300

    dz-1

---
+++ index.js
@@ -0,0 +1,6 @@
+let name = 'Pavel'
+let surname = 'Kokora'
+let age = 15
+let hobby = 'programming'
+
+console.log(`Мене звати ${name} ${surname} мені ${age} років, у вільний час я полюбляю ${hobby}`)
